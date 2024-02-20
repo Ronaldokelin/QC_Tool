f0f3a1a9fb20828f3ff81ef253c6d350c483493d
Arquimedes Miguel <Arquimedes.Miguel@flex.com>
2024-02-20 14:46:06 -0300

    First commit

---
+++ QC_Tool/FormApp.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace QC_Tool
+{
+    partial class FormApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.labelQpm3 = new System.Windows.Forms.Label();
+            this.textBoxQpm3Check = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(56, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(164, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(56, 154);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(164, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // labelQpm3
+            // 
+            this.labelQpm3.AutoSize = true;
+            this.labelQpm3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQpm3.Location = new System.Drawing.Point(53, 42);
+            this.labelQpm3.Name = "labelQpm3";
+            this.labelQpm3.Size = new System.Drawing.Size(106, 20);
+            this.labelQpm3.TabIndex = 2;
+            this.labelQpm3.Text = "QPM 3 Check";
+            // 
+            // textBoxQpm3Check
+            // 
+            this.textBoxQpm3Check.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBoxQpm3Check.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxQpm3Check.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxQpm3Check.Location = new System.Drawing.Point(165, 44);
+            this.textBoxQpm3Check.Name = "textBoxQpm3Check";
+            this.textBoxQpm3Check.Size = new System.Drawing.Size(38, 19);
+            this.textBoxQpm3Check.TabIndex = 3;
+            this.textBoxQpm3Check.Text = "to do";
+            // 
+            // FormApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 559);
+            this.Controls.Add(this.textBoxQpm3Check);
+            this.Controls.Add(this.labelQpm3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "FormApp";
+            this.Text = "QC Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label labelQpm3;
+        private System.Windows.Forms.TextBox textBoxQpm3Check;
+    }
+}
+
